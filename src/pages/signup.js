00a070ae279d6b06ba31114ce94f334ce52b6e2c
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-11-14 14:48:17 -0800

    original routes

---
+++ src/pages/signup.js
@@ -0,0 +1,31 @@
+// Imports
+
+import React, { Component } from 'react'
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Home Page
+
+class signup extends Component {
+
+    render () {
+
+        return (
+            <div>
+                <h1>Signup Page</h1>
+            </div>
+        )
+
+    }
+
+}
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Exports
+
+export default signup
