90ddff3c0d52502b3a99af44a1bde8bdfd571441
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-12-14 23:58:57 -0800

    image upload

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,11 +8,15 @@ import dayjs                            from 'dayjs'
 import Button                           from '@material-ui/core/Button'
 import Typography                       from '@material-ui/core/Typography'
 import MuiLink                          from '@material-ui/core/Link'
+import Tooltip                          from '@material-ui/core/Tooltip'
 import Paper                            from '@material-ui/core/Paper'
+import IconButton                       from '@material-ui/core/IconButton'
 import LocationOn                       from '@material-ui/icons/LocationOn'
 import LinkIcon                         from '@material-ui/icons/Link'
 import CalendarToday                    from '@material-ui/icons/CalendarToday'
+import EditIcon                         from '@material-ui/icons/Edit'
 import { connect }                      from 'react-redux'
+import { logoutUser, uploadImage }      from '../redux/actions/userActions'
 
 
 
@@ -71,22 +75,59 @@ const styles = (theme) => ({
 
 class Profile extends Component {
 
+    handleImageChange = (event) => {
+
+        const image = event.target.files[0]
+        const formData = new FormData()
+        formData.append('image', image, image.name)
+        this.props.uploadImage(formData)
+
+    }
+
+    handleEditPicture = () => {
+
+        const fileInput = document.getElementById('imageInput')
+        fileInput.click()
+
+    }
+
     render() {
 
-        const { 
+        const {
+
             classes, 
             user: { 
                 credentials: { handle, createdAt, imageUrl, bio, website, location }, 
                 loading,
                 authenticated 
             }
+
         } = this.props
 
         let profileMarkup = !loading ? (authenticated ? (
+
             <Paper className={classes.paper}>
                 <div className={classes.profile}>
                     <div className='image-wrapper'>
-                        <img src={imageUrl} alt='profile' className='profile-image'/>
+                        <img 
+                            src={imageUrl} 
+                            alt='profile' 
+                            className='profile-image'
+                        />
+                        <input 
+                            type='file' 
+                            id='imageInput'
+                            hidden='hidden' 
+                            onChange={this.handleImageChange}
+                        />
+                        <Tooltip
+                            title='Edit profile picture'
+                            placement='top'
+                        >
+                            <IconButton onClick={this.handleEditPicture} className='button'>
+                                <EditIcon color='primary'/>
+                            </IconButton>
+                        </Tooltip>
                     </div>
                     <hr/>
                     <div className='profile-details'>
@@ -121,7 +162,9 @@ class Profile extends Component {
                     </div>
                 </div>
             </Paper>
+
         ) : (
+
             <Paper className={classes.paper}>
                 <Typography variant='body2' align='center'>
                     No profile found, please login again
@@ -145,6 +188,7 @@ class Profile extends Component {
                     </Button>
                 </div>
             </Paper>
+            
         )) : (<p>loading...</p>)
 
         return profileMarkup
@@ -161,10 +205,15 @@ const mapStateToProps = (state) => ({
 })
 
 
+const mapActionsToProps = { logoutUser, uploadImage }
+
+
 Profile.propTypes = {
 
-    user:       PropTypes.object.isRequired,
-    classes:    PropTypes.object.isRequired
+    logoutUser:     PropTypes.func.isRequired,
+    uploadImage:    PropTypes.func.isRequired,
+    user:           PropTypes.object.isRequired,
+    classes:        PropTypes.object.isRequired
 
 }
 
@@ -174,4 +223,4 @@ Profile.propTypes = {
 
 // Exports
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -77,6 +77,18 @@ export const logoutUser = () => (dispatch) => {
 }
 
 
+export const uploadImage = (formData) => (dispatch) => {
+
+    dispatch({ type: LOADING_USER })
+    axios.post('/user/image', formData)
+        .then(() => {
+            dispatch(getUserData())
+        })
+        .catch(err => console.log(err))
+
+}
+
+
 const setAuthorizationHeader = (token) => {
 
     const FBIdToken = `Bearer ${token}`
