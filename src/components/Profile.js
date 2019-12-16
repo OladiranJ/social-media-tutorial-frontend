a6e077213344a5e701c1f321ce903cf1529490a3
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-12-15 19:05:28 -0800

    logout button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,6 +15,7 @@ import LocationOn                       from '@material-ui/icons/LocationOn'
 import LinkIcon                         from '@material-ui/icons/Link'
 import CalendarToday                    from '@material-ui/icons/CalendarToday'
 import EditIcon                         from '@material-ui/icons/Edit'
+import KeyboardReturn                   from '@material-ui/icons/KeyboardReturn'
 import { connect }                      from 'react-redux'
 import { logoutUser, uploadImage }      from '../redux/actions/userActions'
 
@@ -91,6 +92,12 @@ class Profile extends Component {
 
     }
 
+    handleLogout = () => {
+
+        this.props.logoutUser()
+
+    }
+
     render() {
 
         const {
@@ -160,6 +167,11 @@ class Profile extends Component {
                         <CalendarToday color='primary'/> {' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
+                    <Tooltip title='Logout' placement='top'>
+                            <IconButton onClick={this.handleLogout}>
+                                <KeyboardReturn color='primary'/>
+                            </IconButton>
+                    </Tooltip>
                 </div>
             </Paper>
 
