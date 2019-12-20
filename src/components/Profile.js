cb0e573dfce6e00f0aec0944fe33c5da1e5949e9
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2019-12-19 19:02:04 -0800

    Navbar Icons and authorization

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,12 +6,11 @@ import withStyles                       from '@material-ui/core/styles/withStyle
 import { Link }                         from 'react-router-dom'
 import dayjs                            from 'dayjs'
 import EditDetails                      from './EditDetails'
+import MyButton                         from '../util/MyButton'
 import Button                           from '@material-ui/core/Button'
 import Typography                       from '@material-ui/core/Typography'
 import MuiLink                          from '@material-ui/core/Link'
-import Tooltip                          from '@material-ui/core/Tooltip'
 import Paper                            from '@material-ui/core/Paper'
-import IconButton                       from '@material-ui/core/IconButton'
 import LocationOn                       from '@material-ui/icons/LocationOn'
 import LinkIcon                         from '@material-ui/icons/Link'
 import CalendarToday                    from '@material-ui/icons/CalendarToday'
@@ -128,14 +127,13 @@ class Profile extends Component {
                             hidden='hidden' 
                             onChange={this.handleImageChange}
                         />
-                        <Tooltip
-                            title='Edit profile picture'
-                            placement='top'
+                        <MyButton 
+                            tip='Edit profile picture' 
+                            onClick={this.handleEditPicture} 
+                            btnClassName='button'
                         >
-                            <IconButton onClick={this.handleEditPicture} className='button'>
-                                <EditIcon color='primary'/>
-                            </IconButton>
-                        </Tooltip>
+                            <EditIcon color='primary'/>
+                        </MyButton>
                     </div>
                     <hr/>
                     <div className='profile-details'>
@@ -168,11 +166,12 @@ class Profile extends Component {
                         <CalendarToday color='primary'/> {' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
-                    <Tooltip title='Logout' placement='top'>
-                            <IconButton onClick={this.handleLogout}>
-                                <KeyboardReturn color='secondary'/>
-                            </IconButton>
-                    </Tooltip>
+                    <MyButton 
+                            tip='Logout' 
+                            onClick={this.handleLogout} 
+                        >
+                            <KeyboardReturn color='secondary'/>
+                        </MyButton>
                     <EditDetails/>
                 </div>
             </Paper>
---
+++ src/util/MyButton.js
@@ -0,0 +1,21 @@
+// Imports
+
+import React        from 'react'
+import Tooltip      from '@material-ui/core/Tooltip'
+import IconButton   from '@material-ui/core/IconButton'
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+// My Button
+
+export default ({ children, onClick, tip, btnClassName, tipClassName }) => (
+
+    <Tooltip title={tip} className={tipClassName}>
+        <IconButton onClick={onClick} className={btnClassName}>
+            {children}
+        </IconButton>
+    </Tooltip>
+
+)
